9a8a1a3b79b14b64a3f5e90c2c3cda3e8f05a184
Anuya Karnik <aakarnik8@gmail.com>
2021-11-29 22:17:30 -0500

    firebase setup and working

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { getDataModel } from './DataModel';
 
 export default function App() {
+// const dataModel = getDataModel();
+// dataModel.addItem({object: 'test1'})
+// dataModel.addItem({object: 'test2'})
+// dataModel.addItem({object: 'test3'})
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
