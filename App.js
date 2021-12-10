1c53295b684ab3b65ee89f3154523510e71de4c0
Anuya Karnik <aakarnik8@gmail.com>
2021-12-09 22:05:33 -0500

    changes to homescreen

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Details" >
+      <Stack.Navigator initialRouteName="Login" >
         <Stack.Screen name="Home"  component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
         <Stack.Screen name="Display" component={DisplayScreen} />
